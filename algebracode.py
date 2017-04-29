dd3cff495e8c0a3811aa3387f855a31af6bebb09
bCubed3 <sf.bcubed@gmail.com>
2017-04-29 13:14:00 -0700

    User input
    Added code to get the user input.

--- algebracode.py
+++ algebracode.py
@@ -1,1 +1,14 @@
 # Algebra Calculator Code
+
+print("Welcome to bCubed's Algebra Calculator.")
+
+user_input = input()
+
+# test if input is an equation or an expression
+
+if(user_input == "help"):
+    print("Help page to come.")
+elif("=" in user_input):
+    print("Equation")
+else:
+    print("Expression")

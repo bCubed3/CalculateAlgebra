1fc1dab37a2a1b69e6a28e73232ddc50f2c2c0e3
bCubed3 <sf.bcubed@gmail.com>
2017-04-29 17:11:46 -0700

    Started factoring

--- algebracode.py
+++ algebracode.py
@@ -7,6 +7,8 @@ def solve_equation(equ):
     # x = c
     if(re.search(r"^[a-z] = \d+$", user_input)):
         return re.search(r"^[a-z]", user_input).group() + " = " + re.search(r"\d+", user_input).group()
+    elif(re.search(r"^\([a-z] [\+\-] \d+\)\([a-z] [\+\-] \d+\) = 0$", user_input)):
+        solve_equation(re.search(r"^\([a-z] [\+\-] \d+\)$", user_input).group()+ " = 0")
     else:
         print("There has been an error.")
 
